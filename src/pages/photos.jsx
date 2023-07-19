ff361edabf4b668cf6f087cdac08529e8d8e983b
Luis Carlos Mencia <luiscarlos@lcmencia.com>
2023-07-19 16:45:51 +0000

    change title etiquetas

--- src/pages/photos.jsx
+++ src/pages/photos.jsx
@@ -128,7 +128,7 @@ const ListPhotosPage = () => {
                         Fecha
                       </th>
                       <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Etiquetas
+                        Etiquetas separadas por comas
                       </th>
                     </tr>
                   </thead>
